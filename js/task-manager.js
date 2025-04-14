7d96f143c887f6bb7468c1f725b49b2552bd21ad
GrassiEmmanuele <emmanuele.grassi@mat.tn.it>
2025-04-14 22:24:44 +0200

    feat: inserita funzione removeTask

--- js/task-manager.js
+++ js/task-manager.js
@@ -1,9 +1,37 @@
-const task = [];
+const tasks = [];
 
-function addTask(){
-    const taskInput = document.getElementById('inputTask');
-    const taskName = taskInput.value.trim();
+function addTask() {
+  const taskInput = document.getElementById("inputTask");
+  const taskName = taskInput.value.trim();
 
-    console.log(taskName);
+  console.log(taskName);
+
+  // controllo che il valore inserito non sia nullo
+  if (taskName === "") {
+    alert("Inserisci un nome valido per l'attività!");
+    return;
+  }
+
+  tasks.push(taskName);
+  renderTasks();
+  taskInput.value = "";
 }
 
+function removeTask(index) {
+    console.log("Indice cancellato:"+index)
+  tasks.splice(index, 1);
+  renderTasks();
+}
+
+function renderTasks() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement("div");
+    taskItem.className = "task-item";
+    taskItem.innerHTML = `<span>${task}</span>
+        <button onclick="removeTask(${index})">Rimuovi</button>`;
+    taskList.appendChild(taskItem);
+  });
+}
